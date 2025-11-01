faa783979a14cfb0271e8fafb0b718e60c3d79e7
Kristian <64616752+Kristian-Y@users.noreply.github.com>
2025-11-01 10:06:30 +0200

    Add sponsor type field to Sponsor model
    Introduces a 'type' CharField to the Sponsor model with choices for gold, silver, bronze, and partner. Includes migration to update the database schema and sets the default type to 'bronze'.

--- fsAdmin/sponsors/models.py
+++ fsAdmin/sponsors/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Sponsor(models.Model):
+  TYPES = [
+    ('gold', 'Gold'),
+    ('silver', 'Silver'),
+    ('bronze', 'Bronze'),
+    ('partner', 'Partner'),
+  ]
   name = models.CharField(max_length=100)
+  type = models.CharField(choices=TYPES, max_length=10, default='bronze')
   logo = models.ImageField(upload_to="sponsor_logos/")
   link = models.URLField(blank=True)
 
